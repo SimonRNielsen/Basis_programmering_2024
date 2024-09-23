8ef07e2acfa623e58f584426164b98f114bacecf
Simon Nielsen <simonrn@gmail.com>
2024-09-23 10:55:46 +0200

    first commit

---
+++ Øvelse1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Hej, jeg er en WriteLine");
+            Console.Beep();
+            Console.Write("Test");
+            Console.ReadLine();
+        }
+    }
+}
