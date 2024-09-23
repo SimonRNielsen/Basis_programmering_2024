8f5830fb80388030eaed089c7dc7779c4014f263
Simon Nielsen <simonrn@gmail.com>
2024-09-23 12:29:26 +0200

    Øvelse 2 (+3)

---
+++ Øvelse2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name!");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hi {name}!");
+            Console.WriteLine("Try typing a number!");
+            int random_number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You just wrote {random_number}!");
+            Console.WriteLine("Press a key!");
+            ConsoleKeyInfo t = Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine($"You Pressed: {t.Key}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Øvelse3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the year: ");
+            Console.WriteLine("You said the year is " + Console.ReadLine() + "!");
+            Console.ReadKey();
+        }
+    }
+}
