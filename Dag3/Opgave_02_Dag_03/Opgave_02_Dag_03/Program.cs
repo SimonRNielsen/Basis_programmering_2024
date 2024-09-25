9dfec1b67da3820775c6aca075cf581d4728417a
Simon Nielsen <simonrn@gmail.com>
2024-09-25 09:21:05 +0200

    commit

---
+++ Dag3/Opgave_02_Dag_03/Opgave_02_Dag_03/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave_02_Dag_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SaySomething();
+          //  SaySomethingWithParameter("Hello");
+
+           int result = ReturnSomething();
+     
+
+            Console.WriteLine(result);
+        }
+
+        static void SaySomething()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static void SaySomethingWithParameter(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        static int ReturnSomething()
+        {
+            return 10;
+        }
+
+        static float Add(float a, float b)
+        {
+            return a + b;
+        }
+
+        static float Divide(float a, float b)
+        {
+            return a / b;
+        }
+
+        static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+        static float Subtract(float a, float b)
+        {
+         
+            return a - b;
+        
+        }
+
+
+        //(X + B * 10) / Y
+        
+        /// <summary>
+        /// Does something
+        /// </summary>
+        /// <param name="x">firs value</param>
+        /// <param name="b">second value</param>
+        /// <param name="y">last value</param>
+        /// <returns>returns the result</returns>
+        static float Something(float x, float b, float y )
+        {
+            return (x + b * 10) / y;
+        }
+
+    }
+}
---
+++ Dag3/Øvelse_01_Dag_03/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    enum Gender {Male, Female, Other }
+
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Gender gender = Gender.Female;
+
+            //Console.WriteLine(gender);
+            Console.WriteLine("Chose your gender!");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i +":" + Enum.Parse(typeof(Gender),i.ToString()));
+            }
+
+            int selection = int.Parse(Console.ReadLine());
+
+            gender = (Gender)selection;
+            Console.WriteLine();
+            Console.WriteLine(gender);
+
+
+            switch (gender)
+            {
+                case Gender.Male:
+                    break;
+                case Gender.Female:
+                    break;
+                case Gender.Other:
+                    break;
+                default:
+                    break;
+            }
+
+            //int i = 0;
+
+            //if (int.TryParse(Console.ReadLine(), out i))
+            //{
+            //    Console.WriteLine("Success!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Parse failed!");
+            //}
+
+            //int i = 300;
+            //byte b = 0;
+
+            //i = b;
+
+            //checked
+            //{
+            //    b = (byte)i;
+            //}
+
+
+            //float f = 2333.223f;
+            //int i = 0;
+            //i = (int)f;
+            //Console.WriteLine(i);
+        }
+    }
+}
