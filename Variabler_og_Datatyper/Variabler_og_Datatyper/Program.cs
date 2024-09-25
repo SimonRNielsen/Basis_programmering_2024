adda2b46397d7a37bf5b20a2171a9e14dd3f83a2
Simon Nielsen <simonrn@gmail.com>
2024-09-25 09:30:49 +0200

    Afleveringsklar

--- Variabler_og_Datatyper/Variabler_og_Datatyper/Program.cs
+++ Variabler_og_Datatyper/Variabler_og_Datatyper/Program.cs
@@ -18,27 +18,28 @@ namespace Variabler_og_Datatyper
             Console.WriteLine("What is your name?");
             playerName = Console.ReadLine();
             Console.WriteLine($"Hi {playerName}!");
-            Restart:
-            Console.WriteLine("Guess a number between 1-100, type in your guess below");
-            playerGuess = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Guess a whole number between 1-100, type in your guess below");
+            playerGuess = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
-            int randomNumber = random.Next(1, 101);            
-            switch (playerGuess)
+            int randomNumber = random.Next(1, 101);
+            if (playerGuess > 100 || playerGuess < 1)
             {
-                case randomNumber == playerGuess:
-
-                    break;
-                case randomNumber < playerGuess:
-
-                    break;
-                case randomNumber > playerGuess:
-
-                    break;
-                case playerGuess > 100:
-
-                    break;
+                Console.WriteLine("That number was not inside the 1-100 range");
             }
-            goto Restart;
+            else if (playerGuess < randomNumber)
+            {
+                Console.WriteLine($"Wrong guess, you were off by {randomNumber - playerGuess}, the number was {randomNumber}");
+            }
+            else if (playerGuess > randomNumber)
+            {
+                Console.WriteLine($"Wrong guess, you were off by {playerGuess - randomNumber}, the number was {randomNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed correct! The number was indeed {playerGuess}!");
+            }
+            Console.WriteLine("Thank you for playing, press any key to close the program");
+            Console.ReadKey();
         }
     }
 }
