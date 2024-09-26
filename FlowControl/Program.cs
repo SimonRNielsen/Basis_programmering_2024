5819082498964ebc2d48e9d5cda484de3d793d6f
SimonRNielsen <simonrn@gmail.com>
2024-09-26 20:30:57 +0200

    Update af QuizProgram

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -11,21 +11,26 @@ namespace FlowControl
     {
         static void Main(string[] args)
         {
-            int player_input = 0;
+            int player_int;
             Random random = new Random();
             bool keep_playing = true;
-            string quit;
-            Console.WriteLine("Welcome to rock, paper, scissor!");
-            while (keep_playing)
+            string player_input;
+            Console.WriteLine("Welcome to rock, paper, scissor!"); //Welcome message outside loop so it only displays once
+            while (keep_playing) //Loop to keep game going
             {
-                int npc_input = random.Next(1, 4);
+                int npc_input = random.Next(1, 4); //Generates a random number between 1-3 every time it's run
                 Console.WriteLine("Type 1 to play rock. Type 2 to play paper. Type 3 to play scissor. Type 'quit' to exit");
-                if (int.TryParse(Console.ReadLine(), out player_input))
+                player_input = PlayerInput(Console.ReadLine()); //Calls function to make letters lower case so only one iteration of "quit" is necessary
+                if (player_input == "quit") //Checks if player wrote "quit" in any way
                 {
-                    switch (player_input)
+                    keep_playing = false; //Ends game loop
+                }
+                else if (int.TryParse(player_input, out player_int)) //Checks player input for numbers
+                {
+                    switch (player_int) //Checks what number the player wrote, has 3 cases for each number, each with 3 underlying scenarios. Also has a default in case player didn't write a number between 1-3
                     {
                         case 1:
-                            switch (npc_input)
+                            switch (npc_input) //Checks computers random number to see what it played
                             {
                                 case 1:
                                     Console.WriteLine("Draw! you both played rock");
@@ -42,13 +47,13 @@ namespace FlowControl
                             switch (npc_input)
                             {
                                 case 1:
-
+                                    Console.WriteLine("Win! you played paper, computer played rock");
                                     break;
                                 case 2:
-
+                                    Console.WriteLine("Draw! you both played paper");
                                     break;
                                 case 3:
-
+                                    Console.WriteLine("Loss! you played paper, computer played scissor");
                                     break;
                             }
                             break;
@@ -56,27 +61,36 @@ namespace FlowControl
                             switch (npc_input)
                             {
                                 case 1:
-
+                                    Console.WriteLine("Loss! you played scissor, computer played rock");
                                     break;
                                 case 2:
-
+                                    Console.WriteLine("Win! you played scissor, computer played paper");
                                     break;
                                 case 3:
-
+                                    Console.WriteLine("Draw! you both played scissor");
                                     break;
                             }
                             break;
+                        default:
+                            Console.WriteLine("You didn't input a valid number");
+                            break;
                     }
                 }
-                else
+                else //Lets the player know they didn't input any usable info
                 {
-                    quit = Console.ReadLine();
-                    if (quit == "quit")
-                    {
-                        keep_playing = false;
-                    }
+                    Console.WriteLine("Invalid input");
                 }
             }
+        }
+        /// <summary>
+        /// A function to make any enter characters lower case
+        /// </summary>
+        /// <param name="player_input"></param>
+        /// <returns>String with only lower case letters</returns>
+        static string PlayerInput(string player_input)
+        {
+            player_input = player_input.ToLower();
+            return player_input;
         }
     }
 }
--- QuizProgram/QuizProgram/Program.cs
+++ QuizProgram/QuizProgram/Program.cs
@@ -8,20 +8,9 @@ namespace QuizProgram
 {
     internal class Program
     {
-        /*enum Hiddenword_hider : int
-        {
-            _ = 0,
-            _ = 1,
-            _ = 2,
-            _ = 3,
-            _ = 4,
-            _ = 5,
-            _ = 6,
-            _ = 7
-        }*/
         static void Main(string[] args)
         {
-            string[] wordLibrary = new string[25]
+            string[] wordLibrary = new string[25]                                               //Creates a "library" using a string-array of 25 words, in this case all are 8 characters long and have a max of 2 duplicate characters (the latter is important for the way the solution is found)
             {
                 "Campfire",
                 "Platform",
@@ -49,71 +38,88 @@ namespace QuizProgram
                 "Baldness",
                 "Wildlife"
             };
-            Random random = new Random();
-            int randomNumber = random.Next(0, 24);
-            string secretword = wordLibrary[randomNumber].ToUpper();
-            char guess;
+            Random random = new Random();                                                       //Creates a random number generater *Praise aRaNGesus!*
+            int randomNumber = random.Next(0, 24);                                              //Generates a random number between and including 0-24
+            string secretword = wordLibrary[randomNumber].ToUpper();                            //Takes random number to call a string from the earlier made library and sets it as the string "secretword"
+            string player_input = string.Empty;
+            char guess = '\0';
             string guesses = string.Empty;
-            char[] hiddenword = new char[8] {'_','_','_','_', '_', '_', '_', '_' };
-            byte lives = 5;
-            int index1 = 8;
-            int index2 = 8;
-            bool win = false;
+            char[] hiddenword = new char[8] { '_', '_', '_', '_', '_', '_', '_', '_' };         //Sets the characters in "hiddenword" (the one that must be equal to "secretword" to win) as "_" for user-readability
+            byte lives = 5;                                                                     //Sets starting lives to 5
+            int index1;
+            int index2;
+            bool win = false;                                                                   //Determines wether or not the player has won by (being a result of a process) comparing "hiddenword" and "secretword"
             Console.WriteLine("Hi! Welcome to a pure text based game of \"Hangman\"");
             Console.WriteLine("You start with 5 lives, you lose 1 for each wrong guess");
             Console.WriteLine("You guess by typing in a single letter and press enter");
             Console.WriteLine("If you guess the 8 letter word, you win!");
-            while (lives > 0 && !win)
+            while (lives > 0 && !win)                                                           //Ensures game keeps looping until player runs out of lives or wins
             {
-                Console.WriteLine(secretword);
-                if (guesses != string.Empty)
+                if (player_input.Length > 1)                                                     //Condition that tells the player he input too many characters - doesn't run on first pass since the string is empty
+                {
+                    Console.WriteLine("Invalid input! Too many characters");
+                }
+                if (guesses != string.Empty)                                                    //Feeds user info on which characters have been picked - doesn't run first time since it's empty, but runs every following time
                 {
                     Console.WriteLine("You have guessed on following letters:");
                     Console.WriteLine(guesses);
                 }
-                Console.WriteLine($"You currently have {lives} lives\n\n\n");
-                Console.WriteLine(hiddenword);
-                guess = Convert.ToChar(Console.ReadLine().ToUpper());
-                if (secretword.Contains(guess))
+                Console.WriteLine($"You currently have {lives} lives\n\n\n");                   //Informs player of the amount of lives remaining and spaces the word to be guessed 3 lines further down
+                Console.WriteLine(hiddenword);                                                  //Displays unknown "_" or known characters of the secret word
+                player_input = Console.ReadLine().ToUpper();                                    //Yields player input in upper casings for consistency
+                if ((player_input.Length == 1) && byte.TryParse(player_input, out byte check))  //Checks if player input a single number and informs player that action isn't allowed
                 {
-                    index1 = secretword.IndexOf(guess);
-                    index2 = secretword.LastIndexOf(guess);
-                    hiddenword[index1] = guess;
-                    hiddenword[index2] = guess;
-                    bool mybool = true;
-                    for (int i = 0; i < hiddenword.Length; i++)
+                    Console.Clear();                                                            //Clears the window to avoid clutter
+                    Console.WriteLine("Numbers are not accepted");
+                }
+                else if (player_input.Length == 1)                                              //Confirms that the user only put in a single character to avoid "Convert.ToChar" to return an exception
+                {
+                    guess = Convert.ToChar(player_input);                                       //Converts user input to a singular uppercase character
+                    if (secretword.Contains(guess))                                             //Checks if the secret word contains the guessed upon character
                     {
-                        if (hiddenword[i] == secretword[i])
+                        index1 = secretword.IndexOf(guess);                                     //Finds the position of the first instance of the guessed character in the string
+                        index2 = secretword.LastIndexOf(guess);                                 //Finds the position of the last (if any) of the guessed character in the string
+                        hiddenword[index1] = guess;                                             //Replaces the "_" in the hiddenword string array with the guessed letter, other right below it replaces an additional one if applicable
+                        hiddenword[index2] = guess;
+                        bool mybool = true;
+                        for (int i = 0; i < hiddenword.Length; i++)
                         {
-
+                            if (hiddenword[i] != secretword[i])                                 //Checks if any characters in the "hiddenword" character-array differs from the characters in the "secretword" string
+                            {
+                                mybool = false;                                                 //If any difference is detected, sets "mybool" as false
+                            }
                         }
-                        else
+                        if (mybool)                                                             //If "mybool" is set to false because of the above check, victory is denied
                         {
-                            mybool = false;
+                            win = true;                                                         //Ends loop even if the player still has lives remaining
                         }
+                        Console.Clear();                                                        //Clears the window to avoid clutter
                     }
-                    if (mybool)
-                    {
-                        win = true;
+                    else
+                    {                                                                           //Removes a life if a guess was wrong, and informs player
+                        Console.Clear();                                                        //Clears the window to avoid clutter
+                        Console.WriteLine("Wrong guess, you lost a life :(");
+                        lives--;
                     }
                 }
-                else
+                if (guesses.Contains(guess))                                                    //Checks if player already guessed on the entered character, and informs the player
                 {
-                    Console.WriteLine("Wrong guess, you lost a life :(");
-                    lives--;
+                    Console.Clear();                                                            //Clears the window to avoid clutter
+                    Console.WriteLine("You already tried this character, guess again");
+                    guesses = guesses.Replace(guess.ToString(), "");                            //Removes the current instance of the character in the string to avoid duplicates for better user-readability
                 }
-                guesses += guess;
-                Console.WriteLine("=========================");
+                guesses += guess;                                                               //Adds the guessed character to those already guessed
+                Console.WriteLine("=========================");                                 //Used as a new "header" for the next loop
             }
-            if (win)
+            if (win)                                                                            //If the loop was ended with lives remaining and guessed the word, this displays the victory message
             {
                 Console.WriteLine($"Congratulations the word was {secretword}, you've won the game!!! Thanks for playing!");
             }
             else
-            {
+            {                                                                                   //This displays the loss message if player runs out of lives
                 Console.WriteLine("Game Over");
             }
-            Console.ReadKey();
+            Console.ReadKey();                                                                  //Prevents console window from closing until a key is pressed
         }
     }
 }
